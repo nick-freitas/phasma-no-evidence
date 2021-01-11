8d7a414b7d2e50551444dfe9aca70cea6c171efd
Naizak <naizakbellemsieh@gmail.com>
2021-01-10 21:36:09 -0500

    Updated ghost description and tests

--- src/app/ghosts.data.ts
+++ src/app/ghosts.data.ts
@@ -1,64 +1,85 @@
 export const GHOSTS = [
   // Non-Freezing
   {
-    name: 'Poltergest',
+    name: 'Poltergeist',
     description:
-      'Sanity drop when player had line of sight of the thrown object (double the sanity drop for each object thrown). Roams a lot.',
+      `A Poltergeist's ability to manipulate objects, specifically doors, can make it easier to identify. However, if the ghost is one that specifically affects multiple light fixtures and other electronics at once, it's more likely to be a Jinn.Poltergeists throw items with higher force than other ghosts.
+      An unwritten quirk of the Poltergeist is that they have the greatest capacity to roam from their Ghost Room, going far to move objects away from its space. In larger maps, this can result in red herrings when looking for Ghost Activity and cause players to waste time searching areas that ultimately do not house the Poltergeist.`,
+    positiveTests: ['Poltergeist Test'],
   },
   {
     name: 'Revenant',
     description:
-      'Smudge sticks during hunt will halt him for 5 seconds (works for every ghost but its most useful for this one).',
+      `Unlike other Ghosts, who will often have a specific target selected when starting a Hunt they will hone in on, Revenants can freely switch targets if there is another player that is closer by - and especially one that is in plain view and available, making its goal of killing players more convenient. 
+      If you are not hiding during a hunt the only way to stay alive is to light a smudge stick while it is chasing you to make it wander somewhere else.`,
     positiveTests: ['Revenant Test'],
   },
   {
     name: 'Jinn',
-    description: '25% sanity drop when within 3m.',
+    description: `Jinns tend to interact with electronics more than any other ghost. They may cause phones to ring, radios to activate, TV's to turn on, or car alarms to go off more often. This also extends to light switches, which may cause unsure investigators to confuse a Jinn for a Mare or Poltergeist; although Mares also have the power to turn lights off, they will not often turn lights back on as Jinn do, and Poltergeists will likely disturb more general objects that aren't light switches such as doors.`,
+    positiveTests: ['Jinn Test'],
   },
   {
     name: 'Spirit',
     description:
-      'Will stop attacking after smudging for 180s instead of the normal 90s.',
+      'With no special powers, and a harsher reaction to Smudge Sticks, Spirits serve as a baseline to measure other Ghosts against. With their behavior ranging from shy to active, passive to hostile, a Spirit can have any number of perceived characteristics. It is this lack of defining traits that can confuse investigators; therefore, a Spirit can be easily mistaken for another entity.',
+    positiveTests: ['Spirit Test'],
   },
   {
     name: 'Oni',
-    description: `Object throwing doesn't decrease sanity`,
+    description: `The Oni is also described as being extremely strong, and it will throw objects around the room with great force when interacting with them. However, these objects are not dangerous to the hunters or their sanity unlike those thrown by Poltergeists.`,
   },
   {
     name: 'Shade',
-    description: 'Can be determined by separating',
+    description: `Noted as being shy ghosts, Shades lower their activity in the presence of multiple investigators. Because of its temperament, it can be harder to find Ghost Activity when sticking together. However, like all ghosts in the game, the Shade is more likely to hunt when the players' Sanity is low.`,
+    positiveTests: ['Shade Test'],
   },
 
   // Freezing
   {
     name: 'Wraith',
-    description: 'Leaves no footprints after touching salt',
+    description: 'Wraiths almost never touch the ground, but this does not apply to the ghost model. Because of this, footprint sounds from a wraith will be rare to non-existent, and they can travel directly through doors without having to open them.',
     requiresFreezingTemp: true,
     positiveTests: ['Wraith Test'],
   },
   {
     name: 'Phantom',
-    description: 'Disapperance when making a photo will not affect hunt',
+    description: 'Looking at a Phantom will considerably drop your Sanity. This refers to any visible manifestations of the Phantom, including during a Hunt.',
     requiresFreezingTemp: true,
+    positiveTests: ['Phantom Test'],
   },
   {
     name: 'Yurei',
-    description: 'Double sanity drain when ghost is visible/hunting w/in 10m',
+    description: `A Yurei will drain sanity at 0.4% per second during a manifestation, this is higher then the usual 0.2% per second from other ghosts.
+    The higher manifestation drain will also happen during a hunt if the player is within 10 meters of the ghost.`,
     requiresFreezingTemp: true,
+    positiveTests: ['Yurei Test'],
   },
   {
     name: 'Mare',
-    description: `Turns off lights/breakers but usually doesn't turn them back on.`,
+    description: `Mares have a higher chance of initiating hunts when players are in darkness, especially if the lights are turned off in the ghost room. 
+    It tends to turn lights and the fuse box off more than any other ghost type when active. An active Mare stalking low-sanity players have the capacity to 
+    initiate hunts within mere moments of each other, allowing them to rival even Demons in aggression. Note that staying in the light does not prevent the 
+    Mare from hunting; it will still be able to initiate Hunts provided it is not currently the Setup Phase, though it will be less aggressive in doing so 
+    when compared to other ghost types.`,
     requiresFreezingTemp: true,
+    positiveTests: ['Mare Test'],
   },
   {
     name: 'Banshee',
-    description: 'Crucifix range effect increase to 5m instead of 3m.',
+    description: `Unlike other ghosts, who will often change their selected target between Hunts, Banshees will always target the same player every time it hunts 
+    until it successfully kills them. While this means other players are safe from the Banshee's power so long as its target is alive, it makes carrying out objectives 
+    by the targeted player more difficult, though at the same time making it much easier for other players. Banshees fear the Crucifix, which boosts the Hunt-stopping 
+    range of one from 3 meters to 5 meters against it.`,
     requiresFreezingTemp: true,
+    positiveTests: ['Banshee Test'],
   },
   {
     name: 'Demon',
-    description: 'Can hunt in 30 second intervals without a reason.',
+    description: `They are generally regarded as the most dangerous type of ghost to encounter; noted for being highly aggressive, it will attack more often than any other 
+    ghost, and usually without warning. They are notorious for initiating hunts frequently, a trait that is exacerbated by lower sanity levels, and compounded by higher 
+    difficulties like Professional where hunts last much longer. Taking all of these factors into account, Demons at their most aggressive will hunt in intervals as short 
+    as half-minutes, forcing the players to spend more total time hiding than investigating.`,
     requiresFreezingTemp: true,
     positiveTests: ['Demon Test'],
   },
--- src/app/tests.data.ts
+++ src/app/tests.data.ts
@@ -1,19 +1,80 @@
 export const TESTS = [
+  // Non-Freezing
+  {
+    name: 'Poltergeist Test',
+    description: 'When items are thrown in front of player, that players sainty drops 2 times the amount of items thrown percent.',
+    positiveTestFor: 'Poltergeist',
+  },
+
   {
     name: 'Revenant Test',
-    description: `During a Hunt, when you are in the Revenant's line of sight, it's speed will greatly increase until it reaches you. The Revenant's speed during a hunt is 2x the speed of other ghosts, and it will not stop until it reaches you (unlike a Jinn).`,
+    description: `During a Hunt, the Revenant can move twice as fast the default speed when it is chasing a player, but it will move 1.5x slower when it isn't.`,
     positiveTestFor: 'Revenant',
   },
+
+  {
+    name: 'Jinn Test',
+    description: `During a hunt the Jinn can move twice as fast the default speed when it is chasing a player until it is within 3 meters of the player and then it will drop back to regular hunting speed.`,
+    positiveTestFor: 'Jinn',
+  },
+
+  {
+    name: 'Spirit Test',
+    description: `Using Smudge Sticks on a Spirit will stop it attacking for 180 seconds instead of 90`,
+    positiveTestFor: 'Spirit',
+  },
+
+  {
+    name: 'Shade Test',
+    description: 'Most activity will only happen when there is only one player in the ghost room. If there are two or more people in the ghost room, it will not initiate a Hunt.',
+    positiveTestFor: 'Shade',
+  },
+
+  // Freezing
+
   {
     name: 'Wraith Test',
-    description: `When it disturbes a salt pile (there is an impression on the salt), it does not leave any footprints behind (there is no footprint visible in black light). To be sure, check for footprints in 3 or more salt piles to determine if it's a Wraith.`,
+    description: `When it disturbes a salt pile (there is an impression on the salt), it does not leave any footprints behind (there is no green footprint visible in black light). To be sure, check for footprints in 3 or more salt piles to determine if it's a Wraith.`,
     positiveTestFor: 'Wraith',
     freezingTempTest: true,
   },
+
+  {
+    name: 'Phantom Test',
+    description: `Taking a photo of the Phantom will make it disappear.`,
+    positiveTestFor: 'Phantom',
+    freezingTempTest: true,
+  },
+
+  {
+    name: 'Yurie Test',
+    description: `Using Smudge Sticks on the Yurei will cause it to not wander around the location for 90 seconds.`,
+    positiveTestFor: 'Yurie',
+    freezingTempTest: true,
+  },
+
+
+  {
+    name: 'Mare Test',
+    description: `If it switchs lights off and never switches lights on, it is likely to be a Mare.`,
+    positiveTestFor: 'Mare',
+    freezingTempTest: true,
+  },
+
+  {
+    name: 'Banshee Test',
+    description: `If it hunts when the player(s) have more than 65% sanity, it's a Banshee.`,
+    positiveTestFor: 'Banshee',
+    freezingTempTest: true,
+  },
+
   {
     name: 'Demon Test',
-    description: `Sanity does not Drop when a question was asked successfully on a Ouiji Board.`,
+    description: `Sanity does not Drop when a question was asked successfully on a Ouiji Board.
+    Or if there is no Ouiji Board on the map if a hunt starts and the player(s) sanity is between 50% - 65%,
+    it's a Demon.`,
     positiveTestFor: 'Demon',
     freezingTempTest: true,
   },
+
 ];
